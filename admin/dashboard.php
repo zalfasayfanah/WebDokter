da4a692ca40e73adf0e243474391884fd2c60a51
KokAzkaImut <azkajpr3@gmail.com>
2025-10-04 14:43:50 +0700

    add file penyakit_home.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,7 @@ class Database
     private $host = 'localhost';
     private $db_name = 'medical_website';
     private $username = 'root';
-    private $password = 'root';
+    private $password = '';
     public $conn;
 
     public function getConnection()
@@ -15,7 +15,7 @@ class Database
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host=" . $this->host . ";port=3307 ;dbname=" . $this->db_name,
                 $this->username,
                 $this->password
             );
